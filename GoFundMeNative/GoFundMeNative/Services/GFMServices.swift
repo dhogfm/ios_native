c36a877b443dd7c2f9739b712119c462062ddcb7
Alan Leatherman <gofundme@Alans-MacBook-Pro.local>
2015-12-29 15:29:35 -0800

    Created base GFMServices file

---
+++ GoFundMeNative/GoFundMeNative/Services/GFMServices.swift
@@ -0,0 +1,13 @@
+//
+//  GFMServices.swift
+//  GoFundMeNative
+//
+//  Created by Alan Leatherman on 12/29/15.
+//  Copyright © 2015 Daniel Ho. All rights reserved.
+//
+
+import UIKit
+
+class GFMServices: NSObject {
+    
+}
