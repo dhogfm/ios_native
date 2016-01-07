04f9d51da7fc718ce369f82ff1f3dd7ba7785ea6
Daniel Ho <dho@gofundme.com>
2016-01-07 10:38:00 -0800

    Added side menu to storyboard

---
+++ GoFundMeNative/GoFundMeNative/GFMSideMenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GFMSideMenuViewController.swift
+//  GoFundMeNative
+//
+//  Created by Daniel Ho on 1/7/16.
+//  Copyright © 2016 Daniel Ho. All rights reserved.
+//
+
+import UIKit
+
+class GFMSideMenuViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
